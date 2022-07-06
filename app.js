a5ff943ee191cd4a13608d6247b06bf76b4074fd
Daeyeon Cho <107440788+daecho8@users.noreply.github.com>
2022-07-06 18:24:56 -0500

    Draft: data got it finally

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-//start
+const $submitBtn = $('#submit');
+const $input = $("input[name='search']")
+console.log($input);
+
+$submitBtn.click(()=>{
+    const userInput = $input.val();
+    $.getJSON(`https://api.aviationapi.com/v1/airports?apt=${userInput}`, (data)=> {
+    console.log(data)
+    // console.log(data['userInput']);
+    console.log(data[userInput][0]);
+    // let result = data[0];
+    // console.log(result);
+
+    // $(".airport-name").text('testing 1');
+    // $(".airport-city").text('testing 2');
+    // $(".airport-state").text(state);
+
+
+    });
+});
+
