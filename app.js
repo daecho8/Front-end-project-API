c0778dfa6b5e1a335d310b98fea9a3e4342cb19b
Daeyeon Cho <107440788+daecho8@users.noreply.github.com>
2022-07-07 00:37:27 -0500

    Weather info added

--- app.js
+++ app.js
@@ -4,18 +4,31 @@ console.log($input);
 
 $submitBtn.click(()=>{
     const userInput = $input.val();
-    $.getJSON(`https://api.aviationapi.com/v1/airports?apt=${userInput}`, (data)=> {
-    console.log(data)
-    // console.log(data['userInput']);
-    console.log(data[userInput][0]);
-    // let result = data[0];
-    // console.log(result);
+    $.getJSON(`https://api.aviationapi.com/v1/airports?apt=${userInput}`, (data)=> {        //airport info
+        // console.log(data)
+        // console.log(data[userInput]);
+        // console.log(data[userInput][0]);
+        const airportName = data[userInput][0]["facility_name"];
+        const city = data[userInput][0]["city"];
+        const state = data[userInput][0]["state"];
+        $(".infoOne").text("AIRPORT INFO");
 
-    // $(".airport-name").text('testing 1');
-    // $(".airport-city").text('testing 2');
-    // $(".airport-state").text(state);
+        $(".airport-name").text(airportName);
+        $(".airport-city").text(city);
+        $(".airport-state").text(state);
+    });
 
+    $.getJSON(`https://api.aviationapi.com/v1/weather/metar?apt=K${userInput}`, (data)=> {      //weather info
+        // console.log(data['K'+ userInput]['wind']);
+        const wind = data['K'+ userInput]['wind'];
+        const ReportTime = data['K'+ userInput]['time_of_obs'];
+        const visibility = data['K'+ userInput]["visibility"];
+        const temp = data['K'+ userInput]["temp"];
+        $(".infoTwo").text("WEATHER INFO");
 
+        $(".weather-reportTime").text("Report Time: " + ReportTime);
+        $(".weather-visiability").text("Visibility: " + visibility);
+        $(".weather-temp").text("Temp: " + temp + " C");
+        $(".weather-wind").text("Wind: " + wind + " knots");
     });
 });
-
