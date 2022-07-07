464216e65bf1a2b078bda80c2e236c954355e145
Daeyeon Cho <107440788+daecho8@users.noreply.github.com>
2022-07-07 15:18:36 -0500

    updated

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
-const $submitBtn = $('#submit');
+const $submitBtn = $('#submit').on( "mouseenter", function() {  //SEARCH BTN color change on
+    $(this).css({
+      "background-color": "green",
+      "font-weight": "bolder"
+    });
+  })
+  .on("mouseleave", function() {
+    var styles = {
+      backgroundColor : "#ddd",
+      fontWeight: ""
+    };
+    $(this).css(styles);
+  });
+
 const $input = $("input[name='search']")
 console.log($input);
 
 $submitBtn.click(()=>{
-    const userInput = $input.val();
+    const userInput = ($input.val()).toUpperCase();
     $.getJSON(`https://api.aviationapi.com/v1/airports?apt=${userInput}`, (data)=> {        //airport info
-        // console.log(data)
-        // console.log(data[userInput]);
-        // console.log(data[userInput][0]);
-        const airportName = data[userInput][0]["facility_name"];
-        const city = data[userInput][0]["city"];
-        const state = data[userInput][0]["state"];
-        $(".infoOne").text("AIRPORT INFO");
+    // console.log(data)
+    // console.log(data[userInput]);
+    // console.log(data[userInput][0]);
+    const airportName = data[userInput][0]["facility_name"];
+    const city = data[userInput][0]["city"];
+    const county = data[userInput][0]["county"];
+    const state = data[userInput][0]["state"];
+    $("#result").css("backgroundColor", "white").css("width", "30%");
+    $(".infoOne").text(`${userInput}` + " Airport");
+    $(".airport-name").text("Full Name: " + airportName);
+    $(".airport-city").text("City: " + city);
+    $(".airport-county").text("County: " + county);
+    $(".airport-state").text("State: " + state);
 
-        $(".airport-name").text(airportName);
-        $(".airport-city").text(city);
-        $(".airport-state").text(state);
-    });
+});
+   
 
     $.getJSON(`https://api.aviationapi.com/v1/weather/metar?apt=K${userInput}`, (data)=> {      //weather info
         // console.log(data['K'+ userInput]['wind']);
@@ -24,11 +41,13 @@ $submitBtn.click(()=>{
         const ReportTime = data['K'+ userInput]['time_of_obs'];
         const visibility = data['K'+ userInput]["visibility"];
         const temp = data['K'+ userInput]["temp"];
-        $(".infoTwo").text("WEATHER INFO");
-
+        $("#weather").css("backgroundColor", "white").css("width", "30%");
+        $(".infoTwo").text(`${userInput}` + " Weather");
         $(".weather-reportTime").text("Report Time: " + ReportTime);
-        $(".weather-visiability").text("Visibility: " + visibility);
+        $(".weather-visiability").text("Visibility: " + visibility + " statue miles");
         $(".weather-temp").text("Temp: " + temp + " C");
         $(".weather-wind").text("Wind: " + wind + " knots");
     });
+
+
 });
